7138ac449735c7e569c45a4094d4b2e5225d8c81
JakeTurnick <jaketurnick@gmail.com>
2025-07-26 11:14:51 -0400

    deck viewer displays dummy data

--- app/deckViewer.tsx
+++ app/deckViewer.tsx
@@ -1,12 +1,38 @@
 import { sharedStyles } from "@/components/ui/sharedStyles";
-import { Link, Stack } from "expo-router";
-import { View } from "react-native";
+import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+
+import * as DataTypes from "@/constants/DataTypes";
+import { exampleDecks } from "@/constants/dummyData";
 
 export default function DeckViewer() {
     return (
-        <View style={sharedStyles.centeredContainer}>
+        <View style={[sharedStyles.centeredContainer, { 
+            display: 'flex',
+            flexDirection: 'row',
+            width: '100%',
+
+        }]}>
             <Stack.Screen options={{ title: 'Deck Viewer' }} />
-            <Link href="./index" style={sharedStyles.text}>Back to Home</Link>
+            {renderDecks(exampleDecks)}
         </View>
     )
+}
+
+function renderDecks(decks: DataTypes.Deck[]) {
+    return decks.map(deck => (
+        <View key={deck.id} style={[sharedStyles.centeredContainer, { 
+            margin: 10,
+            padding: 10,
+            minHeight: "35%",
+            borderWidth: 2,
+            borderColor: '#cccccc',
+            borderStyle: 'solid',
+            borderRadius: 10,
+        }]}>
+            <Text style={sharedStyles.h1Text}>{deck.name}</Text>
+            <Text style={sharedStyles.text}>{deck.description}</Text>
+            
+        </View>
+    ));
 }
